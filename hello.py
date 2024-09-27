93743b6b94da09a934c34ed2c8fcf38168b50d5a
Joey Zhang <zjoey049@gmail.com>
2024-09-26 21:05:50 -0400

    add ctivity 1-3 with date-time

--- hello.py
+++ hello.py
@@ -1,9 +1,38 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime, timezone
 
-@app.route('/')
-def index():
-    return '<h1>Hello World!</h1>'
-@app.route('/user/<name>')
+app = Flask(__name__)  # initialize app object via Flask
+bootstrap = Bootstrap(app)  # pass app into bootstrap
+moment = Moment(app)  # Time
+
+
+# Main APP Routes
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+
+
+@app.route("/user/<name>")
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template(
+        "user.html", name=name, current_time=datetime.now(timezone.utc)
+    )
+
+
+# ERROR HANDLERS
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
+
+
+# TIME
+@app.route("/")
+def index():
+    return render_template("index.html", current_time=datetime.now(timezone.utc))
